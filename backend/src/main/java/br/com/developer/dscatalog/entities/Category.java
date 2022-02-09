08d57eeebf264554a4b56e32c463c132410d5a72
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2022-02-09 17:29:39 -0300

    Entidade Produto

--- backend/src/main/java/br/com/developer/dscatalog/entities/Category.java
+++ backend/src/main/java/br/com/developer/dscatalog/entities/Category.java
@@ -5,13 +5,14 @@ import lombok.*;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "tb_categorias")
+@Table(name = "tb_category")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -32,6 +33,10 @@ public class Category implements Serializable {
     @Getter
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant registroDataUpdate;
+
+    //@ManyToMany(mappedBy = "categories")
+    //private List<Product> produtos = new ArrayList<>();
+
 
     @PrePersist
     public void prePersist(){
---
+++ backend/src/main/java/br/com/developer/dscatalog/entities/Product.java
@@ -0,0 +1,53 @@
+package br.com.developer.dscatalog.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+    @Getter
+    @Setter
+    private String name;
+    @Getter
+    @Setter
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Getter
+    @Setter
+    private Double price;
+    @Getter
+    @Setter
+    private String imgUrl;
+    @Getter
+    @Setter
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant date;
+
+    @Getter
+    @ManyToMany
+    @JoinTable(
+            name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories = new HashSet<>();
+
+
+}
