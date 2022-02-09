5e80b0e47ea3a05b45def8b77d354c1d7c8980b2
Rodrigo Machado <rodrigo.cmachado@gmail.com>
2022-02-09 14:39:37 -0300

    Dados de auditoria

--- backend/src/main/java/br/com/developer/dscatalog/entities/Category.java
+++ backend/src/main/java/br/com/developer/dscatalog/entities/Category.java
@@ -4,19 +4,47 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.Instant;
 
 @Entity
 @Table(name = "tb_categorias")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
 
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Getter
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Getter
+    @Setter
     private String name;
+
+    @Getter
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant dataCriacao;
+
+    @Getter
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant dataAtualizacao;
+
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        dataCriacao = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        dataAtualizacao = Instant.now();
+    }
+
 }
